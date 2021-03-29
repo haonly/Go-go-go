d5eda39849e5aa6855fd3246de4a1ca7e03b466d
parkjaegyun <zerocool@hyundai.com>
2021-03-29 17:58:52 +0900

    4w progressing...

---
+++ zerocool-kor/programmers/cruisecontrol.go
@@ -0,0 +1,17 @@
+/*
+함수 pseudo code 는 대충
+
+CalculateCruiseControlSpeed(button ButtonType , currentSpeed int) {
+
+return int
+
+}
+
+
+ButtonType 은
+UPH (Up Hold - 위로 길게 버튼 누름)
+UPS (Up Short - 위로 짧게 버튼 누름)
+DNH (Down Hold - 아래로 길게 버튼 누름)
+DNS (Down Short - 아래로 짧게 버튼 누름)
+
+*/
