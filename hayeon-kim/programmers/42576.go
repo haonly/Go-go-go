5540ea1c302b8fe94e3ad015ff50697249b8fdd4
hayeon-kim <khayeon0824@gmail.com>
2021-04-01 00:13:52 +0900

    update programmers example

--- hayeon-kim/programmers/42576.go
+++ hayeon-kim/programmers/42576.go
@@ -1,11 +1,25 @@
-func solution(phone_number string) string {
-    var phone_len = len(phone_number)
-    var ret = ""
-    last_four := string(phone_number[phone_len-4:phone_len])
-    
-    for i := 0; i < phone_len-4; i++{
-        ret = ret + "*"
-    }
-    ret = ret + last_four
-    return ret
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func solution(participant []string, completion []string) string {
+	sort.Strings(participant)
+	sort.Strings(completion)
+	
+	for i:=0; i < len(completion); i++{
+		if(participant[i] != completion[i]){
+			return participant[i]
+		}
+	}
+	return participant[len(participant)-1]
+}
+
+func main() {
+	participant := []string{"leo", "kiki", "eden"}
+	completion := []string{"eden", "kiki"}
+	ret := solution(participant, completion)
+	fmt.Println(ret)
 }
